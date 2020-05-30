c5b26b9fce8da409c14cfea35a9eaf976d150000
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-05-30 19:05:05 +0530

    Initial commit

---
+++ Number of integers.py
@@ -0,0 +1,35 @@
+import sys
+import math
+
+
+def getNumberOfIntegers(l,r,k):
+	final=[]	
+
+	for i in range(int(l)+1,int(r)+1):
+		count=0
+		# print(i)
+		for j in str(i):
+			if(int(j) != 0):
+				count+=1
+		final.append(count)
+
+	return(final.count(k))
+
+
+if __name__ == '__main__':	
+	sys.stdin=open("pyIn.txt","r")
+	fout=open("pyOut.txt","w")	
+#---------------------------------
+
+	L = input()
+
+	R = input()
+
+	K = int(input().strip())
+#---------------------------------
+	result=getNumberOfIntegers(L,R,K)
+	fout.write(str(result) + "\n")
+	fout.close()
+
+
+	
