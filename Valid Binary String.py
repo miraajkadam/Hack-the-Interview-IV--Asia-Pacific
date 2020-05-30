6a0aff1bc818f19b5120685a585606af02776e42
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-05-30 22:50:15 +0530

    Update Valid Binary String.py

--- Valid Binary String.py
+++ Valid Binary String.py
@@ -3,11 +3,15 @@ import math
 
 
 def minimumMoves(s,d):
+	count=0
+	flag=False
 	for i in range(len(s)):
 		if(i>=(d-1) and i<=(len(s)-d)):
-			for j in range(i-(d-1),i+(d)):
-				print(j)
-		
+			for j in range(i-(d-1),i+(d-1)+1):
+				if(s[j]==1):
+					flag=True
+	print(flag)
+
 
 
 if __name__ == '__main__':  
